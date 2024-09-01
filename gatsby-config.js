c71b16337401bfc979e7d1d5c2ea0cb9a04da7f4
John Somanza <johnbsomanza@gmail.com>
2024-08-31 19:39:57 -0700

    j to J

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/jlogo.png',
+        icon: 'src/images/Jlogo.png',
       },
     },
     `gatsby-plugin-offline`,
